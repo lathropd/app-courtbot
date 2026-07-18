805b50cd4141b742763d87c0937fce86ac209ed1
Daniel Lathrop <daniel.lathrop@gmail.com>
2026-07-18 14:32:12 -0500

    Add debug echo statements for environment variables
    Added echo statements to output environment variables for debugging.

--- run-python-scrape.sh
+++ run-python-scrape.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 set -euo pipefail
 
+echo $COURTBOT_USER
+echo $COURTBOT_PASSWORD
+echo $GMAIL_USER
+echo $GMAIL_PWD
+
+
 cd "$(dirname "$0")"
 touch "scraper-last-run"
 python3 ./courtbot.py run
